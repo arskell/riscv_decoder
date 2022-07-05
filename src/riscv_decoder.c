29fa8091f48677a31f68d2f3b0f98a6b692981f1
arskell <arskell221@gmail.com>
2022-07-05 20:35:20 +0300

    added help

--- src/riscv_decoder.c
+++ src/riscv_decoder.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
 #include "decode.h"
+#include "stdlib.h"
+
+
+void print_help();
 
 int main(int argc, char** argv){
+    
+    if(argc == 1){
+        print_help();
+        return 0;
+    }
 
-    printf("Hello world\n");
+    long int value = strtoll(argv[1], NULL, 0);
+
+    if(value == 0){
+        print_help();
+        return 0;
+    }
+
+
+    printf("%ld\n", value);
+}
+
+
+void print_help(){
+    printf("Utility for converting given value into riscv instruction\n");
+    printf("Usage riscv_decoder [val]\n");
+    printf("Where val - is riscv instruction\n");
+
 }
